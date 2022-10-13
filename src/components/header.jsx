0b46a07bd148507a2074b557f3c9c7ab7b3a3181
ibqn <evgen.ibqn@gmail.com>
2022-10-13 22:48:56 +0200

    add menu buttons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,9 +4,25 @@ import DownChevronIcon from './down-chevron.svg'
 import SearchIcon from './search.svg'
 import Link from 'next/link'
 
+import HomeIcon from './home.svg'
+import DirectIcon from './direct.svg'
+import NewPostIcon from './new-post.svg'
+import FindPeopleIcon from './find-people.svg'
+import ActivityFeedIcon from './activity-feed.svg'
+
+import ProfileImage from 'images/profile.jpg'
+
+const buttonLinks = [
+  { icon: HomeIcon, href: '#' },
+  { icon: DirectIcon, href: '#' },
+  { icon: NewPostIcon, href: '#' },
+  { icon: FindPeopleIcon, href: '#' },
+  { icon: ActivityFeedIcon, href: '#' },
+]
+
 const Header = () => {
   return (
-    <nav className="flex h-[60px] items-center justify-between border-b px-5">
+    <nav className="flex h-[60px] items-center justify-between border-b px-5 shadow-sm">
       <div className="flex items-end">
         <Link href="/">
           <a>
@@ -18,10 +34,22 @@ const Header = () => {
           <DownChevronIcon />
         </button>
       </div>
-      <div className="hidden h-9 w-56 cursor-text items-center rounded-lg bg-[#efefef] px-4 sm:flex">
+
+      <div className="hidden h-9 max-w-[268px] flex-1 cursor-text items-center rounded-lg bg-[#efefef] px-4 sm:flex">
         <SearchIcon className="mr-3" />
         <button className="cursor-text border-0 bg-inherit text-[#8e8e8e]">
           Search
+        </button>
+      </div>
+
+      <div className="flex gap-x-6 pl-6">
+        {buttonLinks.map((item, index) => {
+          const { icon: Component } = item
+          return <button key={index}>{<Component />}</button>
+        })}
+
+        <button className="cursor-pointer overflow-hidden rounded-full">
+          <Image className="h-6 w-6" src={ProfileImage} />
         </button>
       </div>
     </nav>
