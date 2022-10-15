860fc91ea1b417d0396fc1a3ff531bab51ea2977
ibqn <evgen.ibqn@gmail.com>
2022-10-15 09:19:26 +0200

    show number of notifications

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,7 +14,7 @@ import ProfileImage from 'images/profile.jpg'
 
 const buttonLinks = [
   { icon: HomeIcon, href: '#' },
-  { icon: DirectIcon, href: '#' },
+  { icon: DirectIcon, href: '#', notification: 2 },
   { icon: NewPostIcon, href: '#' },
   { icon: FindPeopleIcon, href: '#' },
   { icon: ActivityFeedIcon, href: '#' },
@@ -22,37 +22,52 @@ const buttonLinks = [
 
 const Header = () => {
   return (
-    <nav className="flex h-[60px] items-center justify-between border-b px-5 shadow-sm">
-      <div className="flex items-end">
-        <Link href="/">
-          <a>
-            <LogoImage className="fill-dark text-dark" />
-          </a>
-        </Link>
+    <div className="sticky top-0 border-b shadow-sm">
+      <nav className="mx-auto flex h-[60px] max-w-5xl items-center justify-between px-5">
+        <div className="flex items-end">
+          <Link href="/">
+            <a>
+              <LogoImage className="fill-dark text-dark" />
+            </a>
+          </Link>
 
-        <button className="flex h-9 items-center p-2">
-          <DownChevronIcon />
-        </button>
-      </div>
+          <button className="flex h-9 items-center p-2">
+            <DownChevronIcon />
+          </button>
+        </div>
 
-      <div className="hidden h-9 max-w-[268px] flex-1 cursor-text items-center rounded-lg bg-[#efefef] px-4 sm:flex">
-        <SearchIcon className="mr-3" />
-        <button className="cursor-text border-0 bg-inherit text-[#8e8e8e]">
-          Search
-        </button>
-      </div>
+        <div className="hidden h-9 max-w-[268px] flex-1 cursor-text items-center rounded-lg bg-[#efefef] px-4 sm:flex">
+          <SearchIcon className="mr-3" />
+          <button className="cursor-text border-0 bg-inherit text-[#8e8e8e]">
+            Search
+          </button>
+        </div>
 
-      <div className="flex gap-x-6 pl-6">
-        {buttonLinks.map((item, index) => {
-          const { icon: Component } = item
-          return <button key={index}>{<Component />}</button>
-        })}
+        <div className="flex gap-x-6 pl-6">
+          {buttonLinks.map((item, index) => {
+            const { icon: Component, notification } = item
+            return (
+              <button className="relative" key={index}>
+                {<Component />}
+                {notification !== undefined && (
+                  <span className="absolute -top-1 -right-2 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs text-white">
+                    {notification}
+                  </span>
+                )}
+              </button>
+            )
+          })}
 
-        <button className="cursor-pointer overflow-hidden rounded-full">
-          <Image className="h-6 w-6" src={ProfileImage} />
-        </button>
-      </div>
-    </nav>
+          <button className="cursor-pointer overflow-hidden rounded-full">
+            <Image
+              className="h-6 w-6"
+              alt="Profile Picture"
+              src={ProfileImage}
+            />
+          </button>
+        </div>
+      </nav>
+    </div>
   )
 }
 
