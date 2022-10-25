232ccce9ac7776fbde711abc7bd1db7eccf33ac5
ibqn <evgen.ibqn@gmail.com>
2022-10-25 20:58:39 +0200

    working on comment part

--- src/components/posts/posts.jsx
+++ src/components/posts/posts.jsx
@@ -4,6 +4,7 @@ import { postsState } from 'recoil-atoms'
 import Image from 'next/future/image'
 import { getUserData } from 'util/user-data'
 import MoreOptionsIcon from './more-options.svg'
+import EmojiIcon from './emoji.svg'
 import Like from './like.svg'
 import Comment from './comment.svg'
 import SharePost from './share-post.svg'
@@ -61,7 +62,7 @@ const Posts = () => {
                 src={avatar}
                 alt="Avatar"
               />
-              <div className="">
+              <div className="flex flex-col">
                 <p className="text-sm font-bold">{username}</p>
                 <p className="text-xs">Original Audio</p>
               </div>
@@ -83,6 +84,23 @@ const Posts = () => {
             </div>
             <div className="p-3 text-sm">
               <PostTitle username={username} postTitle={postTitle} />
+            </div>
+            <div className="p-3 pt-0 text-[10px] uppercase text-[#8e8e8e]">
+              2 days ago
+            </div>
+            <div className="flex items-center border-t p-3">
+              <EmojiIcon className="mr-3" />
+              {/* <input type="text" placeholder="Add a comment..." /> */}
+              <textarea
+                aria-label="Add a comment…"
+                placeholder="Add a comment…"
+                className="h-[18px] grow resize-none appearance-none justify-center border-none text-sm leading-[18px] outline-none"
+                autocomplete="off"
+                autocorrect="off"
+              ></textarea>
+              <button className="ml-auto text-sm font-semibold capitalize text-[#0095F6] opacity-30">
+                post
+              </button>
             </div>
           </div>
         )
